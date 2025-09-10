d15345fb35eee4783b363fb82090b18fef3d9e33
Théo Poizat <hello@zatteo.com>
2025-09-10 15:05:31 +0200

    fix: Allow camera in iframe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <iframe
       id="embeddedApp"
       src={isReady ? urlToLoad : null}
-      allow="clipboard-read; clipboard-write; microphone"
+      allow="clipboard-read; clipboard-write; camera; microphone"
     ></iframe>
   )
 }
